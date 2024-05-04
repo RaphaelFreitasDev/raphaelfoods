eab9f56328fef72cf24ca7cc6cc7c4fc067db11d
RaphaelFreitasDev <freitasraphael.ctba@gmail.com>
2024-05-03 21:08:10 -0300

    feat: add product list component

---
+++ app/_components/products-list.tsx
@@ -0,0 +1,27 @@
+import { Prisma } from "@prisma/client";
+import { db } from "../_lib/prisma";
+import ProductItem from "./product-item";
+
+interface ProductListPops {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+        };
+      };
+    };
+  }>[];
+}
+
+const ProductList = async ({ product }: ProductListPops) => {
+  return (
+    <div className="flex gap-3 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+      {product.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
