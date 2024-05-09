072355de2e9d344856aca1b88f872dc4793be4a1
RaphaelFreitasDev <freitasraphael.ctba@gmail.com>
2024-05-09 15:31:03 -0300

    feat: add link to logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
       <div className="relative h-[30px] w-[100px]">
-        <Image
-          src="/logo.png"
-          alt="Rapha Foods"
-          fill
-          className="object-cover"
-        />
+        <Link href={"/"}>
+          <Image
+            src="/logo.png"
+            alt="Rapha Foods"
+            fill
+            className="object-cover"
+          />
+        </Link>
       </div>
 
       <Button
