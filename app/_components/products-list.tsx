83a31278cebcd2365e0de9274d2f3bc969c6d543
RaphaelFreitasDev <freitasraphael.ctba@gmail.com>
2024-05-09 16:17:35 -0300

    feat: add reccomended products page

--- app/_components/products-list.tsx
+++ app/_components/products-list.tsx
@@ -17,7 +17,11 @@ const ProductList = ({ product }: ProductListPops) => {
   return (
     <div className="flex gap-3 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {product.map((product) => (
-        <ProductItem key={product.id} product={product} />
+        <ProductItem
+          key={product.id}
+          product={product}
+          className="min-w-[150px]"
+        />
       ))}
     </div>
   );
